bc1b3f17526b526fcbc47c420883f166ce18a0b1
lundwall <23060984+lundwall@users.noreply.github.com>
2022-11-07 16:11:56 +0100

    Use LSB benchmamrking tool

--- manual/2d_distribute.cpp
+++ manual/2d_distribute.cpp
@@ -2,16 +2,18 @@
 #include <iostream>
 #include <stdlib.h>
 #include <cstring>
-// #include <liblsb.h>
+#include <liblsb.h>
+#include <time.h>
 
-#define CALIBRATE
-#define NUM_RUNS 1
-#define TIME_REQUIRED 0.5
+#define LSB_RUNS 10
 
 int main(int argc, char** argv)
 {
     int size, rank;
     MPI_Init(&argc, &argv);
+    
+    LSB_Init("test_manual_2d", 0);
+
     MPI_Comm_size(MPI_COMM_WORLD, &size);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
@@ -23,6 +25,11 @@ int main(int argc, char** argv)
         printf("Number of processes should divide array length\n");
         return 0;
     }
+
+    LSB_Set_Rparam_int("rank", rank);
+    LSB_Set_Rparam_int("N", N);
+    LSB_Set_Rparam_int("runs", LSB_RUNS);
+
 
     char* processor_name = new char[256]; int len_processor_name = 0;
     MPI_Get_processor_name(processor_name, &len_processor_name);
@@ -41,81 +48,75 @@ int main(int argc, char** argv)
         newArray[i] = newBuffer + N/P * i;
     }
 
-    int send_array_size[2] = {N/P, N};
-    int recv_array_size[2] = {N, N/P};
-    int subarray_size[2] = {N/P, N/P};
-
     for (int i = 0; i < N/P; i++)
         for (int j = 0; j < N; j++)
             originalArray[i][j] = (N*N/P)*rank+i*N+j;
 
-    double start, end;
-    int num_runs;
-    num_runs = NUM_RUNS;
-    MPI_Request* sendreq = new MPI_Request[(P-1)*(N/P)];
+    MPI_Request* sendreq = new MPI_Request[P-1];
 
-#ifdef CALIBRATE
-    while (num_runs < (1 << 14)) {
-        start = MPI_Wtime();
-        for (int k = 0; k < num_runs; ++k) {
-            int count = 0;
-            for (int i = 0; i < P; i++) {
-                if (rank != i) {
-                    for (int j = 0; j < N/P; j++) {
-                        MPI_Isend(originalBuffer + j*N + i*(N/P), N/P, MPI_INT, i, 0, MPI_COMM_WORLD, &sendreq[count++]);
-                    }
-                }
-            }
+    srand(time(NULL));
 
-            for (int i = 0; i < P; i++) {
-                if (rank != i) {
-                    for (int j = 0; j < N/P; j++) {
-                        MPI_Recv(newBuffer + j*(N/P) + i*(N/P)*(N/P), N/P, MPI_INT, i, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-                    }
-                }
-            }
-
-            MPI_Waitall((P-1)*(N/P), sendreq, MPI_STATUSES_IGNORE);
-        }
-        end = MPI_Wtime();
-
-        double ttime = (double)(end - start);
-
-        if (ttime > TIME_REQUIRED) break;
-        num_runs *= 2;
-    }
-#endif
-
-    start = MPI_Wtime();
-    for(int k = 0; k < num_runs; ++k) {
+    for(int k = 0; k < LSB_RUNS; ++k) {
         int count = 0;
+        LSB_Res();
+
+        // Manual packing
+        int **original_packs = new int*[P];
+        int *original_pack = new int[N*(N/P)];
+        for (int i = 0; i < P; ++i) {
+            if (rank != i) {
+                original_packs[i] = original_pack + (N/P) * (N/P) * i;
+                for (int j = 0; j < N/P; ++j) {
+                    memcpy(&original_packs[i][j * (N/P)], &originalArray[j][i * (N/P)], sizeof(int)*(N/P));
+                }
+            }
+        }
+        int **new_packs = new int*[P];
+        int *new_pack = new int[N*(N/P)];
+        for (int i = 0; i < P; ++i) {
+            if (rank != i) {
+                new_packs[i] = new_pack + (N/P) * (N/P) * i;
+            }
+        }
+
+        // Send
         for (int i = 0; i < P; i++) {
             if (rank != i) {
-                for (int j = 0; j < N/P; j++) {
-                    MPI_Isend(originalBuffer + j*N + i*(N/P), N/P, MPI_INT, i, 0, MPI_COMM_WORLD, &sendreq[count++]);
-                }
+                MPI_Isend(&original_packs[i][0], (N/P)*(N/P), MPI_INT, i, 0, MPI_COMM_WORLD, &sendreq[count++]);
+            }
+        }
+
+        // Self-copy
+        for (int i = 0; i < N/P; i++) {
+            memcpy(&newArray[(N / P) * rank + i][0], &originalArray[i][(N / P) * rank], sizeof(int) * (N / P));
+        }
+
+        // Receive
+        for (int i = 0; i < P; i++) {
+            if (rank != i) {
+                MPI_Recv(&new_packs[i][0], (N/P)*(N/P), MPI_INT, i, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
             }
         }
 
         for (int i = 0; i < P; i++) {
             if (rank != i) {
                 for (int j = 0; j < N/P; j++) {
-                    MPI_Recv(newBuffer + j*(N/P) + i*(N/P)*(N/P), N/P, MPI_INT, i, 0, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-
+                    memcpy(&newArray[i * (N/P) + j][0], &new_packs[i][j * (N/P)], sizeof(int) * (N/P));
                 }
             }
         }
 
-        MPI_Waitall((P-1)*(N/P), sendreq, MPI_STATUSES_IGNORE);
-    }
-    end = MPI_Wtime();
-    std::cout << rank << " time : " << (end - start) / (double)num_runs * 1000000.0 << "us"<< std::endl;
+        MPI_Waitall(P-1, sendreq, MPI_STATUSES_IGNORE);
+        LSB_Rec(k);
 
-    // self-copy
-    for (int i = 0; i < N/P; i++) {
-        memcpy(&newArray[(N / P) * rank + i][0], &originalArray[i][(N / P) * rank], sizeof(int) * (N / P));
+        delete[] original_packs; original_packs = nullptr;
+        delete[] original_pack; original_pack = nullptr;
+        delete[] new_packs; new_packs = nullptr;
+        delete[] new_pack; new_pack = nullptr;
     }
 
+
+    LSB_Finalize();
     MPI_Finalize();
     
     delete[] originalBuffer; originalBuffer = nullptr;
