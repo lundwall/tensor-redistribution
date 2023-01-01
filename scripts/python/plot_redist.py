f4167f7abc9c102325ee85c2f40a8f32d4cd4ff3
lundwall <23060984+lundwall@users.noreply.github.com>
2023-01-01 04:06:58 +0100

    Improve plotting scripts

--- scripts/python/plot_redist.py
+++ scripts/python/plot_redist.py
@@ -6,27 +6,46 @@ import sys
 import os
 
 df_list = []
-columns = ['mode', 'rank', 'time', 'type', 'threads']
 folder = sys.argv[1]
 for filename in os.listdir(os.getcwd() + '/' + folder):
-    df = pd.read_csv(folder + '/' + filename, delim_whitespace=True, comment='#', usecols=columns)
-    df = df[df['type'] == 'running']
+    df = pd.read_csv(folder + '/' + filename, delim_whitespace=True, comment='#')
     df_list.append(df)
 df = pd.concat(df_list)
+df = df[df['type'] == 'running'] # remove syncing runs
 
-df_manual = df[df['mode'] == 'manual']
-df_datatype = df[df['mode'] == 'datatype']
+# Select max across all ranks for each run
+df = df.groupby(['id', 'threads', 'chunks', 'mode'])['time'].agg(['max']).reset_index()
 
-max_rank_manual = df_manual.groupby(['rank'])['time'].median().idxmax()
-max_rank_datatype = df_datatype.groupby(['rank'])['time'].median().idxmax()
+# Plot modes for every thread count
+for t in range(4, 5):
+    plt.figure()
+    df_t = df.copy()
+    df_t = df_t[df_t['threads'] == t]
+    df_t = df_t[df_t['chunks'] == 1]
+    fig, axes = plt.subplots()
+    sns.violinplot(data = df_t, x = 'mode', y = 'max', ax = axes, inner=None)
+    sns.pointplot(data = df_t, x = 'mode', y = 'max', ax = axes, estimator=np.median, color='black', join=False, scale=0.5, errwidth=1, capsize=0.01)
+    plt.savefig(f'redist_modes_t{t}.pdf')
+    plt.close()
 
-df_manual = df_manual[df_manual['rank'] == max_rank_manual]
-df_datatype = df_datatype[df_datatype['rank'] == max_rank_manual]
-
-df = pd.concat([df_manual, df_datatype])
-
+# # Plot threads for manual
+plt.figure()
+df_m = df.copy()
+df_m = df_m[df_m['mode'] == 'manual']
+df_m = df_m[df_m['chunks'] == 1]
 fig, axes = plt.subplots()
-sns.violinplot(data = df, x = 'mode', y = 'time', ax = axes, inner=None)
-sns.pointplot(data = df, x = 'mode', y = 'time', ax = axes, estimator=np.median, color='black', join=False, scale=0.5, errwidth=1, capsize=0.01)
+sns.violinplot(data = df_m, x = 'threads', y = 'max', ax = axes, inner=None)
+sns.pointplot(data = df_m, x = 'threads', y = 'max', ax = axes, estimator=np.median, color='black', join=False, scale=0.5, errwidth=1, capsize=0.01)
+plt.savefig(f'redist_threads_c1.pdf')
+plt.close()
 
-plt.savefig('density_t4_direct_redist.pdf')
+# Plot chunks for manual
+plt.figure()
+df_c = df.copy()
+df_c = df_c[df_c['mode'] == 'manual']
+df_c = df_c[df_c['threads'] == 4]
+fig, axes = plt.subplots()
+sns.violinplot(data = df_c, x = 'chunks', y = 'max', ax = axes, inner=None)
+sns.pointplot(data = df_c, x = 'chunks', y = 'max', ax = axes, estimator=np.median, color='black', join=False, scale=0.5, errwidth=1, capsize=0.01)
+plt.savefig(f'redist_chunks_t4.pdf')
+plt.close()
--- scripts/python/plot_single.py
+++ scripts/python/plot_single.py
@@ -11,18 +11,21 @@ for filename in os.listdir(os.getcwd() + '/' + folder):
     df = pd.read_csv(folder + '/' + filename, delim_whitespace=True, comment='#')
     df_list.append(df)
 df_saved = pd.concat(df_list)
+df_saved = df_saved[df_saved['type'] == 'running'] # remove syncing runs
+
+# Select max across all ranks for each run
+df = df.groupby(['id', 'threads', 'mode'])['time'].agg(['max']).reset_index()
 
 for t in range(1, 9):
     plt.figure()
     df = df_saved.copy()
-    df = df[df['type'] == 'running'] # remove syncing runs
     df = df[df['threads'] == t] # select threads from loop
-    df = df[df['rank'] == 1] # choose receiver timing, more relevant
-    df = df[df['mode'] != 'template'] # template and put_datatype are too big to be nice, so they're excluded
-    df = df[df['mode'] != 'put_datatype']
+    # df = df[df['mode'] != 'template'] # template and put_datatype are too big to be nice, so they're excluded
+    # df = df[df['mode'] != 'put_datatype']
 
     fig, axes = plt.subplots()
     sns.violinplot(data = df, x = 'mode', y = 'time', order=sorted(df['mode'].unique()), ax = axes, inner=None)
     sns.pointplot(data = df, x = 'mode', y = 'time', order=sorted(df['mode'].unique()), ax = axes, estimator=np.median, color='black', join=False, scale=0.5, errwidth=1, capsize=0.01)
 
-    plt.savefig(f'density_t{t}_single.pdf')
+    plt.savefig(f'single_t{t}.pdf')
+    plt.close()
