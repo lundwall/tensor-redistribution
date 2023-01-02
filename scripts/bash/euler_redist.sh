f187e4c29dccbc9842a32629d4c5325c44b412ce
Marc Lundwall <mlundwall@eu-login-40.euler.ethz.ch>
2023-01-02 02:34:39 +0100

    Update scripts to use Euler VII p2

--- scripts/bash/euler_redist.sh
+++ scripts/bash/euler_redist.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
-
 #SBATCH --ntasks=12
 #SBATCH --ntasks-per-socket=1
 #SBATCH --cpus-per-task=8
 #SBATCH --nodes=12
-#SBATCH --mem-per-cpu=25G
-#SBATCH --constraint=EPYC_7H12
+#SBATCH --constraint=EPYC_7763&ibfabric7
 #SBATCH --output=out_redist.txt
+#SBATCH --time=24:00:00
 
 export OMP_NUM_THREADS=8
-srun --cpu-bind=verbose,cores /cluster/home/mlundwall/dphpc/build/redistribute/redistribute
+srun --cpu-bind=cores /cluster/home/mlundwall/dphpc/build/redistribute/redistribute
--- scripts/bash/euler_run_all_5d_20_new.sh
+++ scripts/bash/euler_run_all_5d_20_new.sh
@@ -5,6 +5,6 @@
 #SBATCH --nodes=2
 #SBATCH --mem-per-cpu=4G
 #SBATCH --output=out_mt_5d.txt
-#SBATCH --constraint=EPYC_7H12
+#SBATCH --constraint=EPYC_7763&ibfabric7
 export OMP_NUM_THREADS=8
 srun --cpu-bind=cores ./build/measurement/5d_single_20
