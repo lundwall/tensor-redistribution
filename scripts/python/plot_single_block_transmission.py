689da89b314cf110710132f6d923040a703deb40
Yu-Shan Wei <yuweiy@student.ethz.ch>
2023-01-09 14:47:58 +0100

    add plotting script for single block transmission

--- scripts/python/plot_single_block_transmission.py
+++ scripts/python/plot_single_block_transmission.py
@@ -15,78 +15,70 @@ for filename in os.listdir(os.getcwd() + '/' + folder):
     name_elements = filename.split('.')[1].split('_')
     size_elements = name_elements[-5::]
     try:
-        size_string = size_elements[0] + "_" + size_elements[1] + "_" + size_elements[2] + "_" + size_elements[3] + "_" + size_elements[4]
-        if size_string in size_to_df:
-            size_to_df[size_string].append(df)
+        size_tuple = (int(size_elements[0]), int(size_elements[1]), int(size_elements[2]), int(size_elements[3]), int(size_elements[4]))
+        if size_tuple in size_to_df:
+            size_to_df[size_tuple].append(df)
         else:
-            size_to_df[size_string] = [df]
+            size_to_df[size_tuple] = [df]
     except ValueError:
         print("some size is not integer")
 
 total_df = []
 for key, value in size_to_df.items():
     size_to_df[key] = pd.concat(value)
-    append_column = [key for x in range(len(size_to_df[key].index))]
-    size_to_df[key] = size_to_df[key].assign(size=append_column)
+    size_string = str(key[0]) + "*" + str(key[1]) + "*" + str(key[2]) + "*" + str(key[3]) + "*" + str(key[4])
+    append_column = [size_string for x in range(len(size_to_df[key].index))]
+    size_to_df[key] = size_to_df[key].assign(size_string=append_column)
+    total_size = key[0] * key[1] * key[2] * key[3] * key[4] * 4 / 1000
+    append_column = [total_size for x in range(len(size_to_df[key].index))]
+    size_to_df[key] = size_to_df[key].assign(total_size=append_column)
     total_df.append(size_to_df[key])
 
 df_all = pd.concat(total_df)
 df_all = df_all[df_all['type'] == 'running'] # remove syncing runs
 
-df_t1_c1 = df_all[((df_all['chunk'] == 1) & (df_all['threads'] == 1)) | ((df_all['chunk'].isnull()) & (df_all['threads'] == 1))]
-df_t1_c1 = df_t1_c1.groupby(['id', 'mode', 'size'])['time'].agg(['max']).reset_index()
+# manual packing
+df_t1_c1 = df_all[(df_all['chunk'] == 1) & (df_all['threads'] == 1)]
+df_t1_c1 = df_t1_c1[(df_t1_c1['mode'] == 'datatype') | (df_t1_c1['mode'] == 'manual')]
+df_t1_c1 = df_t1_c1.groupby(['id', 'mode', 'size_string', 'total_size'])['time'].agg(['max']).reset_index()
+df_t1_c1 = df_t1_c1.sort_values(['total_size'])
 
-# For size, chunk = 1, thread = 1
-for key, value in size_to_df.items():
-    df = df_t1_c1[df_t1_c1['size'] == key]
-    plt.figure()
-    fig, axes = plt.subplots()
-    sns.violinplot(data = df, x = 'mode', y = 'max', order=sorted(df['mode'].unique()), ax = axes)
-    # sns.pointplot(data = df, x = 'mode', y = 'max', order=sorted(df['mode'].unique()), ax = axes, estimator=np.median, color='black', join=False, scale=0.5, errwidth=1, capsize=0.01)
-
-    plt.savefig(f'size_{key}.pdf')
-    plt.close()
-
-# Largest size, plot chunk, thread = 1
-df_t1_lsize = df_all[(df_all['size'] == '24_24_24_24_24') & (df_all['threads'] == 1)]
-df_t1_lsize = df_t1_lsize.groupby(['id', 'mode', 'chunk'])['time'].agg(['max']).reset_index()
 plt.figure()
 fig, axes = plt.subplots()
-sns.violinplot(data = df_t1_lsize, x = 'chunk', y = 'max', order=sorted(df_t1_lsize['chunk'].unique()), hue = 'mode',ax = axes)
-# sns.pointplot(data = df_t1_lsize, x = 'chunk', y = 'max', order=sorted(df_t1_lsize['chunk'].unique()), hue = 'mode', ax = axes, estimator=np.median, color='black', join=False, scale=0.5, errwidth=1, capsize=0.01)
+sns.violinplot(data = df_t1_c1, x = 'size_string', y = 'max', hue = 'mode', ax = axes)
+# sns.pointplot(data = df_t1_c1, x = 'mode', y = 'max', order=sorted(df['mode'].unique()), ax = axes, estimator=np.median, color='black', join=False, scale=0.5, errwidth=1, capsize=0.01)
 
-plt.savefig(f'lsize_chunk_24_24_24_24_24.pdf')
+plt.savefig(f'manual_vs_datatype.pdf')
 plt.close()
 
-# Largest size2, plot chunk, thread = 1
-df_t1_lsize = df_all[(df_all['size'] == '12_12_12_12_96') & (df_all['threads'] == 1)]
-df_t1_lsize = df_t1_lsize.groupby(['id', 'mode', 'chunk'])['time'].agg(['max']).reset_index()
-plt.figure()
-fig, axes = plt.subplots()
-sns.violinplot(data = df_t1_lsize, x = 'chunk', y = 'max', order=sorted(df_t1_lsize['chunk'].unique()), hue = 'mode',ax = axes)
-# sns.pointplot(data = df_t1_lsize, x = 'chunk', y = 'max', order=sorted(df_t1_lsize['chunk'].unique()), hue = 'mode', ax = axes, estimator=np.median, color='black', join=False, scale=0.5, errwidth=1, capsize=0.01)
-
-plt.savefig(f'lsize_chunk_12_12_12_12_96.pdf')
-plt.close()
-
-# Largest size, plot thread, chunk = 1
-df_c1_lsize = df_all[(df_all['size'] == '24_24_24_24_24') & (df_all['chunk'] == 1)]
+# manual packing with thread acceleration
+df_c1_lsize = df_all[((df_all['size_string'] == '24*24*24*24*24') & (df_all['mode'] == 'manual') & (df_all['chunk'] == 1)) | ((df_all['size_string'] == '24*24*24*24*24') & (df_all['mode'] == 'datatype') & (df_all['threads'] == 1))]
 df_c1_lsize = df_c1_lsize.groupby(['id', 'mode', 'threads'])['time'].agg(['max']).reset_index()
 plt.figure()
 fig, axes = plt.subplots()
 sns.violinplot(data = df_c1_lsize, x = 'threads', y = 'max', order=sorted(df_c1_lsize['threads'].unique()), hue = 'mode',ax = axes)
 # sns.pointplot(data = df_c1_lsize, x = 'chunk', y = 'max', order=sorted(df_c1_lsize['threads'].unique()), hue = 'mode', ax = axes, estimator=np.median, color='black', join=False, scale=0.5, errwidth=1, capsize=0.01)
 
-plt.savefig(f'lsize_threads_24_24_24_24_24.pdf')
+plt.savefig(f'manual_with_threads_vs_datatype.pdf')
 plt.close()
 
-# Largest size2, plot chunk, chunk = 1
-df_c1_lsize = df_all[(df_all['size'] == '12_12_12_12_96') & (df_all['chunk'] == 1)]
-df_c1_lsize = df_c1_lsize.groupby(['id', 'mode', 'threads'])['time'].agg(['max']).reset_index()
+# manual packing with chunking
+df_t1_lsize = df_all[(df_all['size_string'] == '24*24*24*24*24') & (df_all['threads'] == 1)]
+df_t1_lsize = df_t1_lsize.groupby(['id', 'mode', 'chunk'])['time'].agg(['max']).reset_index()
+df_t1_lsize = df_t1_lsize[(df_t1_lsize['mode'] == 'datatype') | (df_t1_lsize['mode'] == 'manual')]
 plt.figure()
 fig, axes = plt.subplots()
-sns.violinplot(data = df_c1_lsize, x = 'threads', y = 'max', order=sorted(df_c1_lsize['threads'].unique()), hue = 'mode',ax = axes)
-# sns.pointplot(data = df_c1_lsize, x = 'chunk', y = 'max', order=sorted(df_c1_lsize['threads'].unique()), hue = 'mode', ax = axes, estimator=np.median, color='black', join=False, scale=0.5, errwidth=1, capsize=0.01)
+sns.violinplot(data = df_t1_lsize, x = 'chunk', y = 'max', hue = 'mode', ax = axes)
+# sns.pointplot(data = df_t1_lsize, x = 'chunk', y = 'max', order=sorted(df_t1_lsize['chunk'].unique()), hue = 'mode', ax = axes, estimator=np.median, color='black', join=False, scale=0.5, errwidth=1, capsize=0.01)
 
-plt.savefig(f'lsize_threads_12_12_12_12_96.pdf')
+plt.savefig(f'manual_packing_with_chunking_vs_datatype.pdf')
+plt.close()
+
+# MPI one sided
+df_c1_lsize = df_all[(df_all['size_string'] == '24*24*24*24*24') & (df_all['chunk'] == 1) & (df_all['threads'] == 1)]
+df_c1_lsize = df_c1_lsize.groupby(['id', 'mode'])['time'].agg(['max']).reset_index()
+plt.figure()
+fig, axes = plt.subplots()
+sns.violinplot(data = df_c1_lsize, x = 'mode', y = 'max', ax = axes)
+plt.savefig(f'manual_packing_with_one_sided_vs_datatype.pdf')
 plt.close()
